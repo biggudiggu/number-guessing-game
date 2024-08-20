80a4509377a676f4baab699f8401729f2822c1d0
biggudiggu <busragci@hotmail.com>
2024-08-20 19:56:39 +0200

    finished.
    added event listener, reset button, window reload if reset button is pressed...

--- index.js
+++ index.js
@@ -4,16 +4,60 @@ const labelhead = document.getElementById("labelhead");
 const guessLabel = document.getElementById("guessLabel");
 const inputBox = document.getElementById("inputBox");
 const submitBtn = document.getElementById("submitBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 const minNum = 1;
 const maxNum = 100;
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 let attempts = 0;
-let guess;
-let running = false;
 
-guessLabel.textContent = `guess a number between ${minNum} - ${maxNum}`
+guessLabel.textContent = `Guess a number between ${minNum} - ${maxNum}.`
+
+function checkGuess(){
+
+    let value = parseInt(inputBox.value);
+   
+    if(isNaN(value) || value < minNum || value > maxNum) {
+      guessLabel.textContent = "please enter a valid number"
+    }
+    else{
+      attempts++;
+      if(value < answer){
+        guessLabel.textContent = `${value} IS TOO LOW! TRY AGAIN!`;
+      }
+      else if(value > answer){
+        guessLabel.textContent = `${value} IS TOO HIGH! TRY AGAIN!`;
+      }
+      else{
+        guessLabel.textContent = `CORRECT!! THE ANSWER IST ${answer}. YOU NEEDED ${attempts} ATTEMPTS`;
+        inputBox.disabled = true;
+        submitBtn.onclick = function(){
+          window.location.reload();
+        }
+      }
+    }
+    inputBox.value = "";
+  }
+
+  resetBtn.onclick = function(){
+    window.location.reload();
+  }
+
+  submitBtn.addEventListener("click", checkGuess);
+
+  inputBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+      checkGuess();
+    }
+  });
+
+
+
+// with prompt
+/*
+let guess;
+let running = true;
 
 while(running){
   guess = window.prompt(`guess a number between ${minNum} - ${maxNum}`)
@@ -38,4 +82,4 @@ while(running){
       running = false;
     }
   }
-}
+}*/
